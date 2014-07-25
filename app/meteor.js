f824c87a36447f3e58931975450a16a4aa06431a
Ruixin Li <ruixin.li.1994@gmail.com>
2014-07-25 09:21:31 -0400

    added stuff

--- app/meteor.js
+++ app/meteor.js
@@ -1,25 +1,33 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to meteor.";
+    return "Add a player:";
   };
 
   Template.hello.longitude = function() {
     return Session.get("longitude");
-  }
+  };
 
   Template.hello.latitude = function() {
     return Session.get("latitude");
-  }
+  };
 
-
-  Template.hello.events({
+  Template.hello.createPlayer = function() {
+      alert(d3.select(".name").property("value"));
+  };
+    Template.hello.getLocation = function() {
+	navigator.geolocation.getCurrentPosition(function(pos) {
+          Session.set("longitude", pos.coords.longitude);
+          Session.set("latitude", pos.coords.latitude);
+    });
+    }
+  /*Template.hello.events({
     'click input': function () {
         navigator.geolocation.getCurrentPosition(function(pos) {
           Session.set("longitude", pos.coords.longitude);
           Session.set("latitude", pos.coords.latitude);
         });
     }
-  });
+  });*/
 }
 
 if (Meteor.isServer) {
