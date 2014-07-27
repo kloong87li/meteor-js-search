b44c5a1d2442e0a24d5e8f22a257411989092359
Mack Yi <themackyi@gmail.com>
2014-07-27 03:29:39 -0700

    make a starter pokemon

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,3 +1,19 @@
-Meteor.methods({
+if (Meteor.isServer) {
+  Accounts.onCreateUser(function(options, user) {
+  	user.money = 1000;
+  	user.currentlyBusy = false;
+  	user.numpokeballs = 5;
+  	
+    	// We still want the default hook's 'profile' behavior.
+    	if (options.profile)
+      	user.profile = options.profile;
+    	return user;
+  });
 
-})
+  Accounts.onLogin(function(){
+  	// If the user has no pokemon yet, create one
+  	if(Pokemon.find({user_id: Meteor.user._id}).count() === 0){
+  		Meteor.call('createFirstPokemon', Meteor.user._id);
+  	}
+  })
+}
