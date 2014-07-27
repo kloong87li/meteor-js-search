22902db0aeb39787abcdd7ceaa3780843f1e869b
Mack Yi <themackyi@gmail.com>
2014-07-27 03:53:08 -0700

    fix make move, pokemon has userId, not user_id

--- app/collections/battles.js
+++ app/collections/battles.js
@@ -60,10 +60,10 @@ if (Meteor.isServer) {
       var battle = {
         userId1: userId1,
         username1: Meteor.users.findOne({_id: userId1}).username,
-        pokemon1: getFirstPokemon(userId1),
+        pokemonId1: getFirstPokemon(userId1)._id,
         userId2: userId2,
         username2: Meteor.users.findOne({_id: userId2}).username,
-        pokemon2: getFirstPokemon(userId2),
+        pokemonId2: getFirstPokemon(userId2)._id,
         turn: userId1,
         offTurn: userId2,
         isOver: false,
@@ -86,16 +86,15 @@ if (Meteor.isServer) {
         throw new Meteor.Error(400, "It's not your turn");
       }
 
-      if(user._id === battle.player1.userId){
-        var me = battle.player1;
-        var other = battle.player2
+      if(user._id === battle.userId1){
+        var myPokemon = Pokemon.findOne({_id: battle.pokemonId1});
+      	var otherPokemon = Pokemon.findOne({_id: battle.pokemonId2});
       } else {
-        var me = battle.player2;
-        var other = battle.player1
+        var myPokemon = Pokemon.findOne({_id: battle.pokemonId2});
+      	var otherPokemon = Pokemon.findOne({_id: battle.pokemonId1});
       }
 
-      var myPokemon = Pokemon.findOne({_id: me.pokemonId});
-      var otherPokemon = Pokemon.findOne({_id: other.pokemonId});
+
       var move = Move.findOne({name: moveName});
 
       //decrement pp
@@ -148,7 +147,7 @@ if (Meteor.isServer) {
     changePokemon: function(battleId, pokemonId) {
           var battle = Battles.findOne({_id:battleId});
           var pokemon = Pokemon.findOne({_id:pokemonId});
-          if (battle.playerId1 == pokemon.user_id) {
+          if (battle.playerId1 == pokemon.userId) {
               Battles.update({_id:battleId},
                              {$set: {pokemonId1: pokemonId}});
           } else {
